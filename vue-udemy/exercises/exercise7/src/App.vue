5ffa9037bd6f64a00ccbed7cbbc61acad8141047
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2019-11-18 03:08:47 +0500

    exercise7 added

---
+++ vue-udemy/exercises/exercise7/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="currentComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="currentComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="currentComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="currentComponent">
+                    <template v-slot:title>
+                        <h4>I'm a colored one</h4>
+                    </template>
+                    <p>Look at me!</p>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data() {
+            return {
+                currentComponent: Blue
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
