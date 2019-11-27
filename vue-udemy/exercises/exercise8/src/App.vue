31ca67f58651bb3a6c0b8250212e15d96cd031b3
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2019-11-27 10:48:39 +0500

    exercise 8 is done

--- vue-udemy/exercises/exercise8/src/App.vue
+++ vue-udemy/exercises/exercise8/src/App.vue
@@ -1,48 +1,80 @@
 <template>
     <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
-                </div>
-            </div>
-        </form>
-        <hr>
-        <div class="row">
+        <div class="row" v-if="submitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ store }}</p>
                     </div>
                 </div>
             </div>
         </div>
+        <form v-else>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4>Form</h4>
+                        </div>
+                        <div class="panel-body">
+                            <FullName v-model="fullName" />
+                            <label>
+                                Email
+                                <input type="email" v-model="email">
+                            </label>
+                            <label>
+                                Password
+                                <input type="password" v-model="password">
+                            </label>
+                            <p><strong>Store Data?</strong></p>
+                            <label>
+                                Yes
+                                <input type="radio" v-model="store" value="true">
+                            </label>
+                            <label>
+                                No
+                                <input type="radio" v-model="store" value="false">
+                            </label>
+                            <button @click.prevent="submitForm">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
+    import FullName from './FullName.vue';
     export default {
+        data() {
+            return {
+                fullName: '',
+                email: '',
+                password: '',
+                store: false,
+                submitted: false,
+            }
+        },
+        methods: {
+            submitForm() {
+                this.submitted = true;
+            }
+        },
+        components: {
+            FullName
+        }
     }
 </script>
 
-<style>
+<style scoped>
+label {
+    display: block;
+}
 </style>
---
+++ vue-udemy/exercises/exercise8/src/FullName.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <label>
+            First Name
+            <input v-model="firstName" @input="setFullName">
+        </label>
+        <label>
+            Last Name
+            <input v-model="lastName" @input="setFullName">
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['value'],
+    data() {
+        return {
+            firstName:'',
+            lastName:'',
+        }
+    },
+    methods: {
+        setFullName() {
+            this.$emit('input', this.firstName + ' ' + this.lastName);
+        }
+    }
+}
+</script>
