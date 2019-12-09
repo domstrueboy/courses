28446f66a23b142ad3c7fc5fba638efb5a281b24
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2019-12-09 10:18:45 +0500

    I've done some tasks

--- rustlings/exercises/test1.rs
+++ rustlings/exercises/test1.rs
@@ -7,10 +7,16 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn ..... {
+fn calculate_apple_price(price: i32) -> i32 {
+    if (price < 65) {
+        return price * 2;
+    } else {
+        return price;
+    }
+
+}
 
 // Don't modify this function!
 #[test]
