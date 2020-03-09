44190ed6d86e203dffe2d27161cc45c67fd51f61
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-03-09 22:25:50 +0500

    The app has converted to stateful widget

--- flutter-udemy/quizzz/lib/main.dart
+++ flutter-udemy/quizzz/lib/main.dart
@@ -2,17 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+  
   @override
   Widget build(BuildContext context) {
-
-    void answerQuestion() {
-      print('Answer chosen!');
-    }
-
     var questions = [
-      'What\'s your favorite color',
-      'What\'s your favorite animal',
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+      'What\'s your favorite song?',
     ];
     return MaterialApp(
       home: Scaffold(
@@ -21,7 +33,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('The question!'),
+            Text(questions[questionIndex]),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
             RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
             RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
