5f4a9d27c3f33f8e80801bdecd49efc61be13bfe
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-03-09 15:15:07 +0500

    working on

--- flutter-udemy/quizzz/lib/main.dart
+++ flutter-udemy/quizzz/lib/main.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Quizzz!'));
+
+    void answerQuestion() {
+      print('Answer chosen!');
+    }
+
+    var questions = [
+      'What\'s your favorite color',
+      'What\'s your favorite animal',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quizzz'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
+            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
+          ],
+        ),
+      )
+    );
   }
 }
