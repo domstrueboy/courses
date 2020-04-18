ff3f8442d91ab7cf188723099b15eaa53057a0a2
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-04-18 15:39:37 +0500

    wip

--- flutter-udemy/expanse_planner/lib/main.dart
+++ flutter-udemy/expanse_planner/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +19,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 9.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'New House',
+      amount: 99999.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +41,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -34,9 +51,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            child: Text('List of transactions'),
-          )
+          Column(
+              children: transactions.map((transaction) {
+            return Card(
+              child: Text(transaction.title),
+            );
+          }).toList())
         ],
       ),
     );
