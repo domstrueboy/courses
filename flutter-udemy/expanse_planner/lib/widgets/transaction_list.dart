13d89e5fd927e9647f0e53d3a29d74173d52105b
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-04-26 01:38:01 +0500

    transaction list has converted to ListView widget

--- flutter-udemy/expanse_planner/lib/widgets/transaction_list.dart
+++ flutter-udemy/expanse_planner/lib/widgets/transaction_list.dart
@@ -4,68 +4,66 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> transactions;
   TransactionList(this.transactions);
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 340,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((transaction) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$ ${transaction.amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
+      child: ListView.builder(
+        itemBuilder: (ctx, i) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        child: Text(
-                          transaction.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$ ${transactions[i].amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        transactions[i].title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Container(
-                        child: Text(
-                          DateFormat('dd MMMM yyyy').format(transaction.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
+                    ),
+                    Container(
+                      child: Text(
+                        DateFormat('dd MMMM yyyy').format(transactions[i].date),
+                        style: TextStyle(
+                          color: Colors.grey,
                         ),
                       ),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
       ),
     );
   }
