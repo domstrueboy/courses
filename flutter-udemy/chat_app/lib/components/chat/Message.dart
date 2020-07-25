bea4cc94ce08293c37a0e421d18b4504f16fb7a9
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-07-25 19:09:29 +0500

    wip

--- flutter-udemy/chat_app/lib/components/chat/Message.dart
+++ flutter-udemy/chat_app/lib/components/chat/Message.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Message extends StatelessWidget {
+  final Key key;
   final String message;
+  final String username;
+  final bool isMe;
 
-  Message(this.message);
+  Message(this.message, this.isMe, this.username, {this.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+              bottomLeft: isMe ? Radius.circular(15) : Radius.circular(0),
+              bottomRight: isMe ? Radius.circular(0) : Radius.circular(15),
+            ),
           ),
-          width: 140,
+          width: 150,
           padding: EdgeInsets.symmetric(
             vertical: 10,
             horizontal: 16,
@@ -23,11 +32,29 @@ class Message extends StatelessWidget {
             vertical: 4,
             horizontal: 8,
           ),
-          child: Text(
-            message,
-            style: TextStyle(
-              color: Theme.of(context).accentTextTheme.headline1.color,
-            ),
+          child: Column(
+            crossAxisAlignment:
+                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              Text(
+                username,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: isMe
+                      ? Colors.black
+                      : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                  color: isMe
+                      ? Colors.black
+                      : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              ),
+            ],
           ),
         ),
       ],
--- flutter-udemy/chat_app/lib/components/chat/Messages.dart
+++ flutter-udemy/chat_app/lib/components/chat/Messages.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../chat/Message.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance
-          .collection('chat')
-          .orderBy('createdAt', descending: true)
-          .snapshots(),
-      builder: (ctx, chatSnapshot) {
-        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        final messages = chatSnapshot.data.documents;
-        return ListView.builder(
-          reverse: true,
-          itemCount: messages.length,
-          itemBuilder: (ctx, i) => Message(messages[i]['text']),
-        );
+        return StreamBuilder(
+            stream: Firestore.instance
+                .collection('chat')
+                .orderBy('createdAt', descending: true)
+                .snapshots(),
+            builder: (ctx, chatSnapshot) {
+              if (chatSnapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final messages = chatSnapshot.data.documents;
+              return ListView.builder(
+                reverse: true,
+                itemCount: messages.length,
+                itemBuilder: (ctx, i) => Message(
+                  messages[i]['text'],
+                  messages[i]['userId'] == futureSnapshot.data.uid,
+                  messages[i]['username'],
+                  key: ValueKey(messages[i].documentID),
+                ),
+              );
+            });
       },
     );
   }
