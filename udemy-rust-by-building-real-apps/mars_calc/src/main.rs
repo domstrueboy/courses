c4cbb017ab289b2d5ba5e99e10f654e3a06d8a45
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-10-10 18:08:25 +0500

    Weight on Mars calculator is done

--- udemy-rust-by-building-real-apps/mars_calc/src/main.rs
+++ udemy-rust-by-building-real-apps/mars_calc/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter your weight (in kg):");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+
+    let weight_on_mars = calculate_weight_on_mars(weight);
+
+    println!("Weight on Mars: {} kg", weight_on_mars);
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
