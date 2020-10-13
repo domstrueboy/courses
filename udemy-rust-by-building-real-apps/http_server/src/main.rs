c7faa5b644db7e8681c6a28f6cc892d33ead71a0
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-10-14 00:09:28 +0500

    wip

--- udemy-rust-by-building-real-apps/http_server/src/main.rs
+++ udemy-rust-by-building-real-apps/http_server/src/main.rs
@@ -1,20 +1,11 @@
+use http::Request;
+use http::Method;
+use server::Server;
+
+mod http;
+mod server;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
 }
