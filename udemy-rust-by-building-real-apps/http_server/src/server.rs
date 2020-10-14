e182e4f453a5cd6aa9e27561eb39a14789512619
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-10-14 23:52:07 +0500

    wip

--- udemy-rust-by-building-real-apps/http_server/src/server.rs
+++ udemy-rust-by-building-real-apps/http_server/src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
   addr: String,
@@ -12,6 +13,21 @@ impl Server {
   pub fn run(self) {
     println!("Listening on {}", self.addr);
 
-    let listener = TcpListener::bind(&self.addr);
+    let listener = TcpListener::bind(&self.addr).unwrap();
+
+    loop {
+      match listener.accept() {
+        Ok((mut stream, _)) => {
+          let mut buffer = [0; 1024];
+          match stream.read(&mut buffer) {
+            Ok(_) => {
+              println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+            },
+            Err(e) => println!("Failed to read from stream: {}", e),
+          }
+        },
+        Err(e) => print!("Failed to establish connection: {}", e),
+      }
+    }
   }
 }
