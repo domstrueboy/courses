31e43611627ce39e08c76316437d821f38773a63
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2020-11-05 01:35:04 +0500

    http server in Rust is done

--- udemy-rust-by-building-real-apps/http_server/src/website_handler.rs
+++ udemy-rust-by-building-real-apps/http_server/src/website_handler.rs
@@ -1,15 +1,43 @@
+use std::fs;
 use super::server::Handler;
 use super::http::{ Request, Response, StatusCode, Method };
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+  public_path: String,
+}
+
+impl WebsiteHandler {
+  pub fn new(public_path: String) -> Self {
+    Self { public_path }
+  }
+
+  fn read_file(&self, file_path: &str) -> Option<String> {
+    let path = format!("{}/{}", self.public_path, file_path);
+
+    match fs::canonicalize(path) {
+      Ok(path) => {
+        if path.starts_with(&self.public_path) {
+          fs::read_to_string(path).ok()
+        } else {
+          println!("Directory Traversal Attack Attempted: {}", file_path);
+          None
+        }
+      },
+      Err(_) => None,
+    }
+  }
+}
 
 impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
     match request.method() {
       Method::GET => match request.path() {
-        "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+        "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
         "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello</h1>".to_string())),
-        _ => Response::new(StatusCode::NotFound, None),
+        path => match self.read_file(path) {
+          Some(content) => Response::new(StatusCode::Ok, Some(content)),
+          None => Response::new(StatusCode::NotFound, None),
+        }
       }
       _ => Response::new(StatusCode::NotFound, None),
     }
