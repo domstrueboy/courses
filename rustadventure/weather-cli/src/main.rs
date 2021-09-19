285064be7a10508c9af4590b4e07790b2cb769cc
Pavel Bobylyev <pbobylyev@appliedtech.ru>
2021-09-19 12:27:18 +0500

    email 3

--- rustadventure/weather-cli/src/main.rs
+++ rustadventure/weather-cli/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     let api_token = std::env::var("API_TOKEN") // 6ac32f203f2d1a6d06440aa5c659380930d97a4b
         .expect("expected there to be an api token");
-    dbg!(api_token);
+
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next();
+    let args: String = arg_iterator.collect();
+
+    dbg!(args);
 }
