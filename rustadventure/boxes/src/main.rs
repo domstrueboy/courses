5be1b421a9b3f68a0942d8a20cfa916275e38744
pavel bobylyov <p.bobylyov@movavi.com>
2022-03-27 23:06:10 +0500

    changed board color:)

--- rustadventure/boxes/src/main.rs
+++ rustadventure/boxes/src/main.rs
@@ -1,5 +1,46 @@
 use bevy::prelude::*;
 
+mod colors;
+use colors::MATERIALS;
+
+const TILE_SIZE: f32 = 40.0;
+
+#[derive(Component)]
+struct Board {
+    size: u8,
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_startup_system(spawn_board)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands
+        .spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_board(
+    mut commands: Commands,
+) {
+    let board = Board { size: 4 };
+    let physical_board_size =
+        f32::from(board.size) * TILE_SIZE;
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: MATERIALS.board,
+                custom_size: Some(Vec2::new(
+                    physical_board_size, 
+                    physical_board_size
+                )),
+                ..Sprite::default()
+            },
+            ..Default::default()
+        })
+        .insert(board);
 }
