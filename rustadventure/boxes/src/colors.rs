7e84df2fd4869592720fc6d6f267398d8ea2fdfe
pavel bobylyov <p.bobylyov@movavi.com>
2022-04-06 00:19:59 +0500

    added tiles

--- rustadventure/boxes/src/colors.rs
+++ rustadventure/boxes/src/colors.rs
@@ -2,14 +2,14 @@ use bevy::prelude::Color;
 
 pub struct Materials {
     pub board: Color,
-    // pub tile_placeholder: Color,
+    pub tile_placeholder: Color,
     // pub tile: Color,
     // pub none: Color,
 }
 
 pub const MATERIALS: Materials = Materials {
     board: Color::rgb(0.7, 0.7, 0.8),
-    // tile_placeholder: Color::rgb(0.75, 0.75, 0.75),
+    tile_placeholder: Color::rgb(0.75, 0.75, 0.75),
     // tile: Color::rgb(0.9, 0.9, 1.0),
     // none: Color::none,
 };
--- rustadventure/boxes/src/main.rs
+++ rustadventure/boxes/src/main.rs
@@ -1,9 +1,11 @@
 use bevy::prelude::*;
+use itertools::Itertools;
 
 mod colors;
 use colors::MATERIALS;
 
 const TILE_SIZE: f32 = 40.0;
+const TILE_SPACER: f32 = 10.0;
 
 #[derive(Component)]
 struct Board {
@@ -27,8 +29,11 @@ fn spawn_board(
     mut commands: Commands,
 ) {
     let board = Board { size: 4 };
-    let physical_board_size =
-        f32::from(board.size) * TILE_SIZE;
+
+    let physical_board_size = f32::from(board.size) * TILE_SIZE
+        + f32::from(board.size + 1) * TILE_SPACER;
+
+    let offset = - physical_board_size / 2.0 + 0.5 * TILE_SIZE;
 
     commands
         .spawn_bundle(SpriteBundle {
@@ -41,6 +46,30 @@ fn spawn_board(
                 ..Sprite::default()
             },
             ..Default::default()
+        })
+        .with_children(|builder| {
+            for tile in (0..board.size)
+                .cartesian_product(0..board.size)
+            {
+                builder.spawn_bundle(SpriteBundle {
+                    sprite: Sprite {
+                        color: MATERIALS.tile_placeholder,
+                        custom_size: Some(Vec2::new(
+                            TILE_SIZE, 
+                            TILE_SIZE
+                        )),
+                        ..Sprite::default()
+                    },
+                    transform: Transform::from_xyz(
+                        offset + f32::from(tile.0) * TILE_SIZE
+                            + f32::from(tile.0 + 1) * TILE_SPACER,
+                        offset + f32::from(tile.1) * TILE_SIZE
+                            + f32::from(tile.1 + 1) * TILE_SPACER,
+                        1.0,
+                    ),
+                    ..Default::default()
+                });
+            }
         })
         .insert(board);
 }
