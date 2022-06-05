1009b7fc8d95476ab7671e6e2718e257574ebf45
pavel bobylyov <p.bobylyov@movavi.com>
2022-06-05 17:52:52 +0500

    semi-structured-logs solved

---
+++ exercism-rust/rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,31 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+
+    let level_str = match level {
+        LogLevel::Info    => "INFO",
+        LogLevel::Warning => "WARNING",
+        LogLevel::Error   => "ERROR",
+    };
+
+    format!("[{}]: {}", level_str, message)
+}
+pub fn info(message: &str) -> String {
+    log(LogLevel::Info, message)
+}
+pub fn warn(message: &str) -> String {
+    log(LogLevel::Warning, message)
+}
+pub fn error(message: &str) -> String {
+    log(LogLevel::Error, message)
+}
