835aa449080a0d56219cb0c06a7f4662ea6e3838
domstrueboy <domstrueboy@yandex.ru>
2023-08-17 23:41:36 +0500

    largest-series-product is done

--- exercism-rust/rust/largest-series-product/src/lib.rs
+++ exercism-rust/rust/largest-series-product/src/lib.rs
@@ -16,17 +16,20 @@ pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
     }
     for cur in 0..=string_digits.len() - span {
         let chars = string_digits[cur..cur + span].chars();
-        let mut digits = chars.map(|c| c.to_digit(RADIX));
 
-        if digits.all(|d| d.is_some()) {
-            
-        } else {
-            return Err(Error::InvalidDigit('a'));
-        }
+        println!("{:?}", chars);
 
-        let numbers = digits.map(|n| n.unwrap());
+        let not_digit_char = chars.clone().find(
+            |ch|
+            !ch.is_digit(RADIX)
+        );
+
+        if not_digit_char.is_some() { return Err(Error::InvalidDigit(not_digit_char.unwrap())); }
+
+        let numbers = chars.map(|ch| ch.to_digit(RADIX)).map(|d| d.unwrap());
 
         let value = numbers.reduce(|accumulator, digit| accumulator * digit).unwrap();
+
         if value > max {
             max = value;
         }
