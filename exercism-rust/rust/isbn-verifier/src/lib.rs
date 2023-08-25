b529f4b90c598daee95dee0339034295dfac0456
domstrueboy <domstrueboy@yandex.ru>
2023-08-26 00:09:26 +0500

    isbn-verifier is solved

--- exercism-rust/rust/isbn-verifier/src/lib.rs
+++ exercism-rust/rust/isbn-verifier/src/lib.rs
@@ -1,4 +1,27 @@
+const RADIX: u32 = 10;
+const ISBN_LEN: usize = 10;
+
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
-    unimplemented!("Is {isbn:?} a valid ISBN number?");
+
+    let mut numbers = vec![];
+
+    for (i, c) in isbn.chars().enumerate() {
+        if c.is_digit(RADIX) {
+            let digit = c.to_digit(RADIX).unwrap();
+            numbers.push(digit);
+        }
+        if i == isbn.len() - 1 && c == 'X' {
+            numbers.push(10);
+        }
+    }
+    
+    if numbers.len() != ISBN_LEN { return false; }
+
+    let mut acc = 0;
+    for factor in 1..=10 {
+        acc += numbers[ISBN_LEN - factor] * factor as u32;
+    }
+
+    return acc % 11 == 0;
 }
